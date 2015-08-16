4400d017f26ad67eb42ce2608ec67ce8e7ef6ff9
Linus Metzler <linus.metzler@gmail.com>
2015-08-16 16:45:36 +0200

    some necessary adjustments

--- app/Models/InvoiceDesign.php
+++ app/Models/InvoiceDesign.php
@@ -9,28 +9,31 @@ class InvoiceDesign extends Eloquent
 {
     public $timestamps = false;
 
-    public static function getDesigns()
+    public static function getDesigns($forceUtf8 = false)
     {
         $account = Auth::user()->account;
         $designs = Cache::get('invoiceDesigns');
+        $utf8 = $forceUtf8 || $account->utf8_invoices;
 
         foreach ($designs as $design) {
             if ($design->id > Auth::user()->maxInvoiceDesignId()) {
                 $designs->pull($design->id);
             }
-            
-            $design->javascript = $design->pdfmake;
+
+            if ($utf8) {
+                $design->javascript = $design->pdfmake;
+            }
             $design->pdfmake = null;
 
             if ($design->id == CUSTOM_DESIGN) {
-                if ($account->custom_design) {
+                if ($utf8 && $account->custom_design) {
                     $design->javascript = $account->custom_design;
                 } else {
                     $designs->pop();
                 }
             }
         }
-        
+
         return $designs;
     }
 }
